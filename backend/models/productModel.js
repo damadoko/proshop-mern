d37195ab904dd3c33c0c399ac4a9ae05e7c8927c
damadoko <lmtrietct@gmail.com>
2022-09-01 14:08:56 +0700

    Add mongoose model for user, product, order

---
+++ backend/models/productModel.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    rating: {
+      type: Number,
+      require: true,
+    },
+    comment: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
